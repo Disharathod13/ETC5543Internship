88d64c5ec5209fb65a3ce79483e51e4ed4d6edd3
Disha Rathod <drat0009@student.monash.edu>
2024-07-26 11:31:57 +1000

    making a CSV with percentile where ranking is as per the state

--- IRSADsa1-popscore.R
+++ IRSADsa1-popscore.R
@@ -12,3 +12,12 @@ rankingwaus <- sa1_popscore |>
   rename(`Percentile within Australia` = `Percentile...7`)
 
 
+# Save the data to a CSV file
+write_csv(rankingwaus, "SA1_rankingwaus.csv")
+
+rankingwstate <- sa1_popscore |> 
+  select('2021 Statistical Area Level 1 (SA1)', 'State', 'Usual Resident Population', 'Score', 'Percentile...12') %>%
+  rename(`Percentile within State` = `Percentile...12`)
+
+# Save the data to a CSV file
+write_csv(rankingwstate, "SA1_rankingwstate.csv")
